1b94ca10b02b021ac2a26d1d9bcb086cedc47e3b
lyhhh <17610603221@163.com>
2021-08-08 03:42:35 +0800

    add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import styled from 'styled-components'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Title className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,9 +19,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </Title>
     </div>
   )
 }
 
 export default App
+
+const Title = styled.div`
+  color: pink;
+`
