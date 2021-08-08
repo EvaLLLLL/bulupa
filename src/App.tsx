f2e616268fd28030094a6bee6e93bc94da286468
lyhhh <17610603221@163.com>
2021-08-08 23:34:19 +0800

    add antd & router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import styled from 'styled-components'
+import 'antd/dist/antd.css'
+import {LayoutDemo} from './Layout'
 
 function App() {
   return (
-    <div className="App">
-      <Title className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </Title>
-    </div>
+    <LayoutDemo/>
   )
 }
 
 export default App
-
-const Title = styled.div`
-  color: pink;
-`
